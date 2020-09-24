009338c76ee37d75b5b5dd4dbc0f269bbed2fab4
Brandon Humphry <brandonhumphry@gmail.com>
2020-09-23 19:34:17 -0700

    added handleChange function to checklist item to switch from check to unchecked. Currently not working

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,16 +1,59 @@
-import React from 'react';
-import MyToDo from './components/MyToDo';
+import React, {Component} from 'react';
 import "./App.css"
 import WelcomeMessage from './components/WelcomeMessage';
+import ToDoItem from "./ToDoItem";
+import todosData from "./todosData";
 
-function App() {
+// function App() {
 
-  return (
-    <div>
-      <WelcomeMessage/>
-      <MyToDo/>
-    </div>
-  );
+//   return (
+//     <div>
+//       <WelcomeMessage/>
+//       <MyToDo/>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+    // console.log("changed", id)
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <ToDoItem key={item.id} item={item} handleChange={this.handleChange}/>
+    ))
+
+    return (
+      <div className="to-do-list">
+        <WelcomeMessage/>
+        <h1>Brandon's To Do List</h1>
+        {todoItems}
+      </div>
+    );
+  }
 }
+
+// export default MyToDo
+
 
 export default App;
--- react-to-do/src/components/MyToDo.js
+++ react-to-do/src/components/MyToDo.js
@@ -1,28 +1,33 @@
-import React, { Component } from "react";
-import ToDoItem from "./ToDoItem.js";
-import todosData from "../todosData";
+import React from "react";
+import ToDoItem from "../ToDoItem.js";
+import todosData from "./todosData";
 
-// function MyToDo() {
-//     const ToDoItems = todosData.map(item => <ToDoItem key={item.id} item={item}/>)
-
-//     return (
-//       <div className="to-do-list">
-//         <h1>Brandon's To Do List</h1>
-//         {ToDoItems}
-//       </div>
-//     );
-//   }
 class MyToDo extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       todos: todosData
-    };
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
   }
 
   render() {
     const ToDoItems = this.state.todos.map(item => (
-      <ToDoItem key={item.id} item={item} />
+      <ToDoItem key={item.id} item={item} handleChange={this.handleChange}/>
     ));
 
     return (
@@ -34,4 +39,4 @@ class MyToDo extends React.Component {
   }
 }
 
-export default MyToDo;
+export default MyToDo
