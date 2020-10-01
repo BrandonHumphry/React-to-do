886174fcb9d2a545de3b76800f1e80a8ac1026f9
Brandon Humphry <brandonhumphry@gmail.com>
2020-10-01 16:41:11 -0700

    Updates

---
+++ precache-manifest.e38eca0484875dd64c3d2558b8fe3e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22a98255779f661ffdc1c5520b39ce6",
+    "url": "/React-to-do/index.html"
+  },
+  {
+    "revision": "ce7f4a8172bc18f6103d",
+    "url": "/React-to-do/static/css/main.55512a42.chunk.css"
+  },
+  {
+    "revision": "159e36aa5b3db2cbe5a3",
+    "url": "/React-to-do/static/js/2.2f4156a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-to-do/static/js/2.2f4156a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7f4a8172bc18f6103d",
+    "url": "/React-to-do/static/js/main.5bc28760.chunk.js"
+  },
+  {
+    "revision": "ce474150e60fa4d44004",
+    "url": "/React-to-do/static/js/runtime-main.ef0abc2b.js"
+  }
+]);
